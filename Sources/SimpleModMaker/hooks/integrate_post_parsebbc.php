ee9f98f3617b9bfe8ed84893d4b1f60da9edf942
Bugo <cheetah@bk.ru>
2023-12-08 18:10:33 +0500

    Add a new hook template - integrate_post_parsebbc

---
+++ Sources/SimpleModMaker/hooks/integrate_post_parsebbc.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'params' => [
+		'message' => ['string', true],
+		'smileys' => ['boolean', true],
+		'cache_id' => ['string', true],
+		'parse_tags' => ['array', true],
+	],
+	'body' => []
+];
