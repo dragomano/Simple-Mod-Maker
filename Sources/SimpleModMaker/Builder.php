9d3a959f7799483a46cc807d7e6995fe7f950c66
Bugo <cheetah@bk.ru>
2024-01-04 00:14:09 +0500

    Update version

--- Sources/SimpleModMaker/Builder.php
+++ Sources/SimpleModMaker/Builder.php
@@ -8,10 +8,10 @@ declare(strict_types=1);
  * @package Simple Mod Maker
  * @link https://github.com/dragomano/Simple-Mod-Maker
  * @author Bugo <bugo@dragomano.ru>
- * @copyright 2022-2023 Bugo
+ * @copyright 2022-2024 Bugo
  * @license https://opensource.org/licenses/BSD-3-Clause BSD
  *
- * @version 0.6.1
+ * @version 0.7
  */
 
 namespace Bugo\SimpleModMaker;
