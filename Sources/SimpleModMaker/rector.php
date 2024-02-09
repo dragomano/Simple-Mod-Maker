95c43451d9389f77e30c33976cdfd02e78c15406
Bugo <cheetah@bk.ru>
2024-02-09 05:47:10 +0500

    Update dependencies

--- Sources/SimpleModMaker/rector.php
+++ Sources/SimpleModMaker/rector.php
@@ -2,28 +2,17 @@
 
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
-use Rector\Set\ValueObject\SetList;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/*',
-    ]);
-
-    $rectorConfig->skip([
-        __DIR__ . '/vendor/*',
-        JsonThrowOnErrorRector::class,
-        RecastingRemovalRector::class,
-    ]);
-
-    $rectorConfig->parallel(360);
-
-    $rectorConfig->importNames();
-    $rectorConfig->importShortClasses(false);
-
-    $rectorConfig->sets([
-        SetList::DEAD_CODE,
-        LevelSetList::UP_TO_PHP_74
-    ]);
-};
+return RectorConfig::configure()
+	->withPaths([
+		__DIR__ . '*',
+	])
+	->withSkip([
+		__DIR__ . '/vendor/*',
+		RecastingRemovalRector::class,
+	])
+	->withParallel(360)
+	->withIndent(indentChar: "\t")
+	->withImportNames(removeUnusedImports: true)
+	->withPreparedSets(deadCode: true)
+	->withPhpSets();
