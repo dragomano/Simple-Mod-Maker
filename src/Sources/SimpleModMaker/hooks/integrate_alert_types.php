4523676f16d8161676a08ece645b66a96f8bece7
Bugo <cheetah@bk.ru>
2024-11-23 10:05:50 +0500

    Update templates of integrate_alert_types and integrate_fetch_alerts

--- src/Sources/SimpleModMaker/hooks/integrate_alert_types.php
+++ src/Sources/SimpleModMaker/hooks/integrate_alert_types.php
@@ -1,9 +1,25 @@
 <?php
 
+$type = "\$alert_types['{$snake_name}']['some_action'] = [
+	'alert' => 'yes', // or 'never'
+	'email' => 'yes', // or 'never'
+	'permission' => [
+		'name'     => 'some_permission',
+		'is_board' => false,
+	]
+];";
+
 return [
 	'params' => [
 		'alert_types' => ['array', true],
 		'group_options' => ['array', true],
 	],
-	'body' => []
+	'body' => $context['smm_skeleton']['smf_target_version'] !== '3.0' ? [
+		"global \$txt;" . PHP_EOL,
+		"\$txt['alert_group_{$snake_name}'] = \$txt['{$snake_name}_title'];" . PHP_EOL,
+		$type
+	] : [
+		"Lang::\$txt['alert_group_{$snake_name}'] = Lang::\$txt['{$snake_name}_title'];" . PHP_EOL,
+		$type
+	],
 ];
